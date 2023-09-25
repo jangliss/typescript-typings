ab0c24b6508be1d37f67160eb0aaff460e03d4aa
s791373 <s791373@emn.com>
2023-09-25 07:08:37 -0400

    Added a few more properties

--- greasyfork.d.ts
+++ greasyfork.d.ts
@@ -2,5 +2,10 @@ declare var GM_info: {
     script: {
         name: string;
         version: string;
+        downloadURL: string;
+        updateURL: string;
     }
+    scriptUpdateURL: string;
 }
+
+declare var GM_xmlhttpRequest: XMLHttpRequest;
